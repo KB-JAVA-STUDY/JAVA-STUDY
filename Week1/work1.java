3e3da7e367f3e7d5a57b47a99bdbeef22ab1ee56
yeomsky95 <yeomsky95@gmail.com>
2025-03-23 20:17:54 +0900

    '1주차과제'

---
+++ Week1/work1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+//import java.lang.*;
+
+public class work1 {
+    public static int check(int last) {
+        int ln = 0;
+        for (int j = 1; j < last; j++) {
+            if ((j * j) > last)
+                ln = j;
+        }
+        return ln;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        int dec = 0;
+        int[] numArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            numArr[i] = sc.nextInt();
+        }
+        int[] disArr = Arrays.stream(numArr).distinct().toArray();
+        int max = Arrays.stream(disArr).max().getAsInt();
+        int last = check(max);
+        int len = disArr.length;
+        for (int k = 0; k < len; k++) {
+            if (disArr[k] == 1)
+                continue;
+            for (int l = 2; l < last; l++) {
+                if ((disArr[k] % l) == 0 && (disArr[k] != l))
+                    count++;
+            }
+            if (count == 0)
+                dec++;
+            count = 0;
+        }
+        System.out.println(dec);
+        sc.close();
+    }
+}
